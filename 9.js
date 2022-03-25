67909be9d400d84521c89d212db029c6d8c3178c
Pavel Tyukin <tyukin.pavel@yandex.ru>
2022-03-25 21:29:21 +0300

    6

--- 9.js
+++
@@ -1,2 +0,0 @@
-// const has all the awesome features that let has, with the added bonus that variables declared using const are read-only. They are a constant value, which means that once a variable is assigned with const, it cannot be reassigned:
-//
